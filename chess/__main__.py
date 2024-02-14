53e9aef34659339e0e10e75a0572b21e7902ec40
Tyson Schwartz <scratelt@yahoo.com>
2024-02-14 00:03:17 -0500

    created a Board class, including draw/find/left click event functions

--- chess/__main__.py
+++ chess/__main__.py
@@ -8,10 +8,13 @@ GRID_SIZE = 8
 
 class Square:
     square_length = BOARD_LENGTH / GRID_SIZE
+    files = "ABCDEFGH"
+    ranks = "87654321"
 
     def __init__(self, col, row):
         self.row = row
         self.col = col
+
         self.x1 = col * self.square_length
         self.y1 = row * self.square_length
         self.x2 = self.x1 + self.square_length
@@ -22,6 +25,61 @@ class Square:
         canvas.create_rectangle(
             self.x1, self.y1, self.x2, self.y2, fill=self.color
         )
+
+    def __str__(self) -> str:
+        return self.position
+
+    def __repr__(self) -> str:
+        return f"Square({self.row}, {self.col}, {self.color})"
+
+    @property
+    def position(self):
+        return f"{self.files[self.col]}{self.ranks[self.row]}"
+
+
+class Board(tk.Canvas):
+    def __init__(self, master, width: int, height: int):
+        self.master = master
+        self.width = width
+        self.height = height
+
+        super().__init__(master, width=width, height=height)
+
+        self.squares = self._init_squares()
+        self.bind("<Button-1>", self.left_click)
+
+    def draw(self):
+        self.draw_squares()
+
+    def find_square(self, row: int, col: int) -> Square:
+        for square in self.squares:
+            if row == square.row and col == square.col:
+                return square
+
+    def draw_squares(self):
+        for square in self.squares:
+            square.draw(self)
+
+    def left_click(self, event):
+        print(f"event x: {event.x}, event y: {event.y}")
+        col = int(event.x // Square.square_length)
+        row = int(event.y // Square.square_length)
+        square = self.find_square(row, col)
+
+        old_color = square.color
+        square.color = "yellow"
+        self.draw()
+        square.color = old_color
+
+    @staticmethod
+    def _init_squares():
+        squares = []
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
+                square = Square(col, row)
+                squares.append(square)
+
+        return squares
 
 
 def main() -> None:
@@ -35,30 +93,11 @@ def main() -> None:
     window.title("Chess")
     window.geometry(f"{BOARD_LENGTH}x{BOARD_LENGTH}")
 
-    canvas = tk.Canvas(window, width=BOARD_LENGTH, height=BOARD_LENGTH)
+    # canvas = tk.Canvas(window, width=BOARD_LENGTH, height=BOARD_LENGTH)
+    board = Board(window, width=BOARD_LENGTH, height=BOARD_LENGTH)
 
-    # loop - increment x and y by board_length / grid_size, then keep y constant and increment x until x is board_length, then reset x to 0. increment Y again and repeat until y is board_length for last loop.
-
-    upper_left = (0, 0)
-    lower_right = (BOARD_LENGTH, BOARD_LENGTH)
-
-    canvas.create_rectangle(*upper_left, *lower_right, fill="blue")
-
-    # canvas.create_rectangle(x1, y1, x2, y2, fill="color")
-
-    # canvas.create_rectangle(0, 0, 400, 400, fill="red")
-    # canvas.create_rectangle(400, 0, 800, 400, fill="black")
-    # canvas.create_rectangle(0, 400, 400, 800, fill="red")
-    # canvas.create_rectangle(400, 400, 800, 800, fill="black")
-    squares = []
-    for row in range(GRID_SIZE):
-        for col in range(GRID_SIZE):
-            square = Square(col, row)
-            square.draw(canvas)
-            squares.append(square)
-
-    # row x grid_size + col
-    canvas.pack()
+    board.draw()
+    board.pack()
 
     # stuff that goes in window
 
